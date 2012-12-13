79fb7d20cf89e46121ec5485802d54b6441ee57b
Zach Tibbitts <zachtib@gmail.com>
2012-12-12 19:54:08 -0500

    remove unused import

--- src/com/zachtib/thefuckingweather/MainActivity.java
+++ src/com/zachtib/thefuckingweather/MainActivity.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Locale;
-import java.util.concurrent.ExecutionException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
